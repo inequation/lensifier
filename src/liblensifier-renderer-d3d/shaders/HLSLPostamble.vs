1cfc5b41eb63ef55ba98775aa66de6d3009e467d
Leszek Godlewski <github@inequation.org>
2014-02-22 22:09:13 +0100

    flip V coords in HLSL to account for the difference between OpenGL and D3D conventions

--- src/liblensifier-renderer-d3d/shaders/HLSLPostamble.vs
+++ src/liblensifier-renderer-d3d/shaders/HLSLPostamble.vs
@@ -5,7 +5,8 @@ PixelInput main(in VertexInput _VI)
 	LensifierMain(_VI);
 	PixelInput _PI;
 	_PI.Position = _VI.Position;
-	_PI.TexCoords = _VI.TexCoords;
+	// flip V to convert from OpenGL convention
+	_PI.TexCoords = _VI.TexCoords * vec4(1.0, -1.0, 1.0, 1.0) + vec4(0.0, 1.0, 0.0, 0.0);
 	_PI.VertexColour = _VI.Colour;
 	return _PI;
 }
--- src/liblensifier-renderer-d3d/shaders/HLSLPreamble.cs
+++ src/liblensifier-renderer-d3d/shaders/HLSLPreamble.cs
@@ -7,16 +7,46 @@ SamplerState LensifierSamplerBilinear
 	AddressV = Wrap;
 };
 vec4 Sample1D(Texture1D Texture, float Coord) {return Texture.Sample(LensifierSamplerBilinear, Coord);}
-vec4 Sample2D(Texture2D Texture, vec2 Coord) {return Texture.Sample(LensifierSamplerBilinear, Coord);}
-vec4 Sample3D(Texture3D Texture, vec3 Coord) {return Texture.Sample(LensifierSamplerBilinear, Coord);}
-//vec4 Sample4D(Texture4D Sampler, vec4 Coord) {return Texture.Sample(LensifierSamplerBilinear, Coord);}
+vec4 Sample2D(Texture2D Texture, vec2 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec2(1.0, -1.0) + vec2(0.0, 1.0);
+	return Texture.Sample(LensifierSamplerBilinear, Coord);
+}
+vec4 Sample3D(Texture3D Texture, vec3 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec3(1.0, -1.0, 1.0) + vec3(0.0, 1.0, 0.0);
+	return Texture.Sample(LensifierSamplerBilinear, Coord);
+}
+/*vec4 Sample4D(Texture4D Sampler, vec4 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec4(1.0, -1.0, 1.0, 1.0) + vec4(0.0, 1.0, 0.0, 0.0);
+	return Texture.Sample(LensifierSamplerBilinear, Coord);
+}*/
 )
 "\n#else\n"
 STRINGIFY(
 vec4 Sample1D(sampler1D Sampler, float Coord) {return tex1D(Sampler, Coord);}
-vec4 Sample2D(sampler2D Sampler, vec2 Coord) {return tex2D(Sampler, Coord);}
-vec4 Sample3D(sampler3D Sampler, vec3 Coord) {return tex3D(Sampler, Coord);}
-//vec4 Sample4D(sampler4D Sampler, vec4 Coord) {return tex4D(Sampler, Coord);}
+vec4 Sample2D(sampler2D Sampler, vec2 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec2(1.0, -1.0) + vec2(0.0, 1.0);
+	return tex2D(Sampler, Coord);
+}
+vec4 Sample3D(sampler3D Sampler, vec3 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec3(1.0, -1.0, 1.0) + vec3(0.0, 1.0, 0.0);
+	return tex3D(Sampler, Coord);
+}
+/*vec4 Sample4D(sampler4D Sampler, vec4 Coord)
+{
+	// flip V to convert from OpenGL convention
+	Coord = Coord * vec4(1.0, -1.0, 1.0, 1.0) + vec4(0.0, 1.0, 0.0, 0.0);
+	return tex4D(Sampler, Coord);
+}*/
 )
 "\n#endif\n"
 STRINGIFY(
@@ -31,7 +61,7 @@ vec4 Blend(vec4 A, vec4 B, float Alpha) {return lerp(A, B, Alpha);}
 
 struct PixelInput
 {
-    vec4 Position       : SV_POSITION;
+	vec4 Position       : SV_POSITION;
 	vec4 TexCoords		: TEXCOORD0;
 	vec4 VertexColour	: COLOR0;
 };
