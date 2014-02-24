988e2c48b7d4f1b4b950bd143e143f6bd93e63a8
Leszek Godlewski <github@inequation.org>
2014-02-24 01:37:45 +0100

    Sample2DMS for GLSL

--- src/liblensifier-renderer-gl/shaders/GLSLPreamble.cs
+++ src/liblensifier-renderer-gl/shaders/GLSLPreamble.cs
@@ -1,10 +1,12 @@
 STRINGIFY(
 #define Sampler1D sampler1D
 \n#define Sampler2D sampler2D
+\n#define Sampler2DMS sampler2D
 \n#define Sampler3D sampler3D
 //\n#define Sampler4D sampler4D
 vec4 Sample1D(sampler1D Sampler, float Coord) {return texture1D(Sampler, Coord);}
 vec4 Sample2D(sampler2D Sampler, vec2 Coord) {return texture2D(Sampler, Coord);}
+vec4 Sample2DMS(sampler2D Sampler, vec2 Coord) {return texture2D(Sampler, Coord);}
 vec4 Sample3D(sampler3D Sampler, vec3 Coord) {return texture3D(Sampler, Coord);}
 //vec4 Sample4D(sampler4D Sampler, vec4 Coord) {return texture4D(Sampler, Coord);}
 float Saturate(float In) {return clamp(In, 0.0, 1.0);}
